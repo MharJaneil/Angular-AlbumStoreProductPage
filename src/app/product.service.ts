50cd6fe4a8dac6c047029191fd8e0a369492f9a2
Your Name <mharjaneil.castillo.c@bulsu.edu.ph>
2023-04-11 18:00:06 +0800

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@angular/core";
+
 import { Http, Response } from "@angular/http";
+
 import "rxjs/add/operator/map";
+
 import { Album } from "./album";
+
 import { Observable } from "rxjs/Observable";
+
 import { Product } from "./product";
+
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
