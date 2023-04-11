65b40e5a5cc38f08bb217d0e8c38293d03d055d5
Your Name <mharjaneil.castillo.c@bulsu.edu.ph>
2023-04-11 17:55:22 +0800

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+
 import { Product } from "../product";
+
 import { ProductService } from "../product.service";
+
 @Component({
   selector: "app-product-list",
   templateUrl: "./product-list.component.html",
